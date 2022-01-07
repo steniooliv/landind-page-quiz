55cb2e259e8a5abd95b20c7bf3072d682034d7ae
Stenio Oliveira <steniooliv@gmail.com>
2022-01-07 00:10:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Template Next App with Typescript
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import styles from "../styles/home.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <h1 className={styles.h1}>
+      Hello World
+    </h1>
+  )
+}
+
+export default Home
