402cd153e7aa36cf2986335fdfb51e4bb3b38c93
Stenio Oliveira <steniooliv@gmail.com>
2022-01-13 18:23:08 -0300

    update product list

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,15 +1,18 @@
 import styles from "./styles.module.scss";
 
 import data from "../../../data.json";
+import Link from "next/link";
 
 export function ProductList() {
   return (
-    <ul>
-      {data.products.map(e => {
-        <li>
-          {e}
+    <ul className={styles.list}>
+      {data.products.map(product => (
+        <li key={product.id}>
+          <Link key={product.id} href={`/product/${product.id}`}>
+          {product.name}
+          </Link>
         </li>
-      })}
+      ))}
     </ul>
   );
 }
