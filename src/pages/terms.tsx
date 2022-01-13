fa893c4c9e2140fb4b3023d890421ee13cea34c1
Stenio Oliveira <steniooliv@gmail.com>
2022-01-13 17:54:13 -0300

    create use terms page

---
+++ src/pages/terms.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+import styles from '../styles/terms.module.scss';
+
+const Terms : NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Termos de Uso</title>
+      </Head>
+
+      <main className={styles.main}>
+
+        <h2 className={styles.h2}>Termos de Uso</h2>
+
+        <p className={styles.terms}>
+          Termos de Uso
+          Seus Direitos e Termos para uso das informações estão detalhados aqui
+
+          Existe este Termo de uso a fim de demonstrar o nosso firme comprometimento sobre como você pode utilizar todas as informações deste Site.
+          O acesso a este site representa a aceitação expressa e irrestrita dos termos de uso abaixo descritos. Se você não concorda com os termos, por favor, não acesse nem utilize este website.
+          O acesso a este site representa a aceitação expressa e irrestrita dos termos de uso abaixo descritos. Se você não concorda com os termos, por favor, não acesse nem utilize este website.
+          O visitante poderá usar este site apenas para finalidades lícitas. Este espaço não poderá ser utilizado para para publicar, enviar, distribuir ou divulgar conteúdos ou informação de caráter difamatório, obsceno ou ilícito, inclusive informações de propriedade exclusiva pertencentes a outras pessoas ou empresas, bem como marcas registradas ou informações protegidas por direitos autorais, sem a expressa autorização do detentor desses direitos. Ainda, o visitante não poderá usar este site para obter ou divulgar informações pessoais, inclusive endereços na Internet, sobre os usuários do site.
+          A equipe deste site empenha-se em manter a qualidade, atualidade e autenticidade das informações do site, mas seus criadores e colaboradores não se responsabilizam por eventuais falhas nos serviços ou inexatidão das informações oferecidas.
+          O usuário não deve ter como pressuposto que tais serviços e informações são isentos de erros ou serão adequados aos seus objetivos particulares. Os criadores e colaboradores tampouco assumem o compromisso de atualizar as informações, e reservam-se o direito de alterar as condições de uso ou preços dos serviços e produtos oferecidos no site a qualquer momento.
+          O acesso a este site é gratuito. Este site poderá criar áreas de acesso exclusivo aos seus clientes ou para terceiros especialmente autorizados.
+          Os criadores e colaboradores deste site poderão a seu exclusivo critério e em qualquer tempo, modificar ou desativar o site, bem como limitar, cancelar ou suspender seu uso ou o acesso. Também estes Termos de Uso poderão ser alterados a qualquer tempo. Visite regularmente esta página e consulte os Termos então vigentes. Algumas disposições destes Termos podem ser substituídas por termos ou avisos legais expressos localizados em determinadas páginas deste site.
+          Informações enviadas pelos usuários e colaboradores
+          Qualquer material, informação, artigos ou outras comunicações que forem transmitidas, enviadas ou publicadas neste site serão considerados informação não confidencial, e qualquer violação aos direitos dos seus criadores não será de responsabilidade da equipe deste site. É terminantemente proibido transmitir, trocar ou publicar, através deste website, qualquer material de cunho obsceno, difamatório ou ilegal, bem como textos ou criações de terceiros sem a autorização do autor. A equipe deste site reserva-se o direito de restringir o acesso às informações enviadas por terceiros aos seus usuários.
+          Este site poderá, mas não é obrigado, a monitorar, revistar e restringir o acesso a qualquer área no site onde usuários transmitem e trocam informações entre si, incluindo, mas não limitado a, salas de chat, centro de mensagens ou outros fóruns de debates, podendo retirar do ar ou retirar o acesso a qualquer destas informações ou comunicações. Porém, a equipe deste site não é responsável pelo conteúdo de qualquer uma das informações trocadas entre os usuários, sejam elas lícitas ou ilícitas.
+          Limitação da Responsabilidade
+          Os materiais são fornecidos neste website sem nenhuma garantia explícita ou implícita de comercialização ou adequação a qualquer objetivo específico. Em nenhum caso este site ou os seus colaboradores serão responsabilizados por quaisquer danos, incluindo lucros cessantes, interrupção de negócio, ou perda de informação que resultem do uso ou da incapacidade de usar os materiais. A equipe deste site não garante a precisão ou integridade das informações, textos, gráficos, links e outros itens dos materiais.
+          A equipe deste site não é responsável pela violação de direitos autorais decorrente de informações, documentos e materiais publicados neste website, comprometendo-se a retirá-los do ar assim que notificado da infração.
+          Erros e falhas
+          Os documentos, informações, imagens e gráficos publicados neste site podem conter imprecisões técnicas ou erros tipográficos. Em nenhuma hipótese a equipe deste site e/ou seus respectivos fornecedores serão responsáveis por qualquer dano direto ou indireto decorrente da impossibilidade de uso, perda de dados ou lucros, resultante do acesso e desempenho do site, dos serviços oferecidos ou de informações disponíveis neste site. O acesso aos serviços, materiais, informações e facilidades contidas neste website não garante a sua qualidade.
+          Links para sites de terceiros
+          Os sites apontados não estão sob o controle deste site que não é responsável pelo conteúdo de qualquer outro website indicado ou acessado por meio deste site reserva-se o direito de eliminar qualquer link ou direcionamento a outros sites ou serviços a qualquer momento. Este site não endossa firmas ou produtos indicados, acessados ou divulgados através deste website, tampouco pelos anúncios aqui publicados, reservando-se o direito de publicar este alerta em suas páginas eletrônicas sempre que considerar necessário.
+          Direitos autorais e propriedade intelectual
+          Os documentos, conteúdos e criações contidos neste website pertencem aos seus criadores e colaboradores. A autoria dos conteúdo, material e imagens exibidos no diabetescontrolada.com.br é protegida por leis nacionais e internacionais. Não podem ser copiados, reproduzidos, modificados, publicados, atualizados, postados, transmitidos ou distribuídos de qualquer maneira sem autorização prévia e por escrito do proprietários desse site.
+          As imagens contidas neste website são aqui incorporadas apenas para fins de visualização, e, salvo autorização expressa por escrito, não podem ser gravadas ou baixadas via download. A reprodução ou armazenamento de materiais recuperados a partir deste serviço sujeitará os infratores às penas da lei.
+          O nome deste site, seu logotipo, o nome de domínio para acesso na Internet, bem como todos os elementos característicos da tecnologia desenvolvida e aqui apresentada, sob a forma da articulação de bases de dados, constituem marcas registradas e propriedades intelectuais privadas e todos os direitos decorrentes de seu registro são assegurados por lei. Alguns direitos de uso podem ser cedidos pelo proprietários do site, em contrato ou licença especial, que pode ser cancelada a qualquer momento se não cumpridos os seus termos.
+          As marcas registradas deste site só podem ser usadas publicamente com autorização expressa. O uso destas marcas registradas em publicidade e promoção de produtos deve ser adequadamente informado.
+        </p>
+
+      </main>
+
+    </>
+  );
+}
+
+export default Terms;
+
