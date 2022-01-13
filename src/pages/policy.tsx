dd6dbbdeedb6e61d02463d782493cef366c0c1c8
Stenio Oliveira <steniooliv@gmail.com>
2022-01-13 17:54:06 -0300

    create policy page

---
+++ src/pages/policy.tsx
@@ -0,0 +1,87 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+import styles from '../styles/policy.module.scss';
+
+const Policy: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Política de Privacidade</title>
+      </Head>
+
+      <main className={styles.main}>
+
+        <h2 className={styles.h2}>
+          Política de Privacidade
+        </h2>
+
+        <p className={styles.terms}>
+          Política de Privacidade
+          A segurança das suas informações é muito importante para nós.
+
+          Seu perfil pessoal
+          Quando pedimos informações sobre você, o nosso principal interesse é oferecer serviços mais personalizados. Nós respeitamos a sua privacidade e não a vendemos, nem alugamos, tampouco compartilhamos seu nome e informações pessoais com outras organizações.
+
+          Ao fornecer suas informações pessoais e foto, você compreende e consente à equipe deste site o uso da sua imagem, foto, vídeo e comentários. Essas informações estão disponíveis em seu perfil público e em nossa comunidade, onde você também verá as dos outros membros. Você tem a opção de negar o uso das suas informações ao atualizar suas escolhas em “Privacidade” – “Configurações de Conta”. De tempos em tempos, nós poderemos usar algumas informações de clientes em nossos materiais de marketing. Todavia, nunca faremos isso sem antes entrar em contato com você e receber um consentimento por escrito.
+
+          Medidas de segurança
+          A segurança de seus dados pessoais é importante para nós. Seguimos os padrões aceitos no ramo para proteger as suas informações pessoais que nos são enviadas tanto durante a transmissão de dados quanto após o recebimento deles. Quando você digita informações sigilosas (como seu cartão de crédito) em nossos formulários, elas são criptografadas para a transmissão, usando a tecnologia SSL (camada de sockets protegida).
+
+          Se você descobrir uma cobrança fraudulenta em sua fatura, feita em nome da equipe deste site, por favor, contate-nos e reporte o caso imediatamente para a administradora do cartão de acordo com os procedimentos padrões deles. O site faz uso de fóruns, murais e salas de bate-papo. Por favor, lembre-se de que qualquer informação que você expuser por esses meios se torna pública. Sempre tenha cautela com as informações pessoais que você fornece a qualquer pessoa nesses contextos.
+
+          Também insistimos na verificação da senha ao fazer login, para que você e seus dados estejam protegidos contra acessos sem autorização. Se você parar de usar o computador por muito tempo sem encerrar a sua sessão na escola, ela será encerrada automaticamente após certo tempo.
+
+          Apenas funcionários que precisem da informação para realizar certas tarefas (como cobrança ou atendimento ao aluno, por exemplo) têm acesso a informações pessoais. Os servidores nos quais armazenamos suas informações são mantidos em ambiente seguro.
+
+          Apesar das práticas e procedimentos de segurança, como acontece com toda e qualquer rede de computadores na internet, a equipe deste site não pode garantir a segurança de seus dados pessoais ou de quaisquer outros dados passados pela internet e, portanto, não poderá ser responsabilizada por falhas de segurança.
+
+          Nenhum método de transmissão de dados ou armazenamento eletrônico na internet é 100% seguro. Deste modo, não podemos garantir a segurança total de seus dados.
+
+          Comunicados do site
+          Para enviar a você as lições diárias (E-mail), precisamos de seu endereço de e-mail. Como assinante delas, você será adicionado em nossa lista para envio de e-mails eventuais com atualizações de serviços, ofertas especiais e promoções deste site.
+
+          Você pode cancelar a sua inscrição ao receber os e-mails a qualquer momento. Basta clicar no link “Remover e-mail da lista” que aparece no rodapé do e-mail que enviamos. Enviaremos anúncios relacionados a serviços raramente, apenas quando for necessário.
+
+          Por exemplo, se o nosso serviço for temporariamente suspenso para manutenção, é possível que enviemos um e-mail para avisá-lo. Em geral, não é possível optar por não receber este último tipo de comunicação por não serem e-mails promocionais. Se você não quer receber os e-mails de aviso, você pode solicitar o descadastramento.
+
+          Proteção de informação de menores Estamos comprometidos a proteger a privacidade dos menores. Esse e todos nossos sites são voltados ao público em geral e não são feitos para atrair crianças com menos de 13 anos.
+
+          Não coletamos, nem solicitamos informações pessoais de menores de 13 anos, tampouco enviamos e-mails solicitando dados pessoais, a menos que tenhamos a permissão dos pais ou guardiões legais da criança. Inscrição e cadastro para assinatura deste site oferece cursos e produtos voltados para a área de saúde e bem estar.
+
+          Para fazer um desses cursos, aproveitar o período grátis ou tornar-se usuário de nossa comunidade, você precisa se tornar membro deste site. Durante a inscrição, coletamos informações como seu nome e e- mail.
+
+          Também pedimos dados demográficos, como idade e sexo, para criarmos seu perfil. Você também pode ter de fornecer seu cartão de crédito, caso o serviço que pediu seja pago. Essa informação é criptografada em nosso servidor seguro.
+
+          Como membro, você será adicionado à nossa base para receber e- mails eventuais com informações de serviços, ofertas especiais e promoções da Universidade da Franquia.
+
+          Web Beacons
+          Usamos uma tecnologia chamada clear gifs (também conhecida como web beacons ou web bugs), que ajuda a gerenciar melhor o conteúdo do site, informando qual funciona melhor. Os clear gifs são pequenos gráficos com um identificador único e têm função similar aos cookies – são usados para rastrear os movimentos online de usuários da internet. Porém, ao contrário dos cookies, que ficam no computador do usuário, os clear gifs são incorporados na própria página e são do tamanho de um ponto. Não utilizamos os clear gifs que contêm informações pessoais de nossos clientes. Usamos clear gifs em nossos e-mails com formato HTML para sabermos quais são abertos pelos destinatários. Assim, podemos medir a eficácia de certas comunicações e de nossas campanhas. Em alguns e-mails, colocamos conteúdo com links para clicar e ser redirecionado para o site do Chave do Coração do Homem. Quando os alunos e/ou clientes clicam em um desses links, passam por um servidor à parte antes de chegar à página de destino. Rastreamos os dados de cliques para determinar o interesse por cada assunto e medirmos a eficiência de nossas comunicações com o aluno. Caso prefira não ser rastreado assim, não clique em links de texto ou imagens em seus e-mails.
+
+          Podemos fornecer seus dados a empresas que prestem serviços e que nos ajudem em nossas atividades, como o processamento de pagamentos ou a oferta de atendimento. Essas empresas estão autorizadas a usar seus dados apenas se necessário para prestação de serviços a nós. Também podemos passar seus dados:
+
+          – conforme exigido por lei para atender a intimações ou processos legais similares;
+
+          – quando acreditarmos, de boa fé, que a quebra de sigilo é necessária para protegermos nossos direitos, proteger sua segurança ou a segurança de outros, investigar fraudes ou atender a um pedido do governo;
+
+          – caso a Universidade da Franquia se envolva numa fusão, aquisição, ou seja vendida em termos ou ao todo, você será notificado por e-mail, ou por um aviso bem claro em nosso site, de qualquer mudança nesse sentido, ou no uso de suas informações pessoais, bem como de qualquer nova situação que envolva os seus dados pessoais sendo divulgados a terceiros, a fim de que isso nunca seja feito sem consentimento.
+
+          Cookies
+          “Cookies” são pequenas informações que gravamos no browser que você utiliza para navegar ou no hard drive. Nós e nossos parceiros usamos os cookies para nos lembrarmos de informações pessoais quando você visita nosso site. Por exemplo, caso opte por visualizar nosso site em francês, armazenaremos essa informação nos cookies para que mostremos o site sempre em francês para você. Também usamos os cookies para gravar seu login, a fim de facilitar seu acesso ao site. A maioria dos browsers permite que você opte por não permitir que cookies sejam armazenados em seu computador. Usamos cookies para a sessão e cookies persistentes. O cookie de sessão expira assim que você fecha o browser. O cookie persistente continua em seu computador por um período maior. Além disso, usamos um prestador de serviços para rastrear o comportamento de visitantes em nosso site. Contudo, não temos acesso a seus cookies, tampouco os controlamos. As informações coletadas pela empresa prestadora de serviços são anônimas e usadas para melhorar nossos serviços e campanhas de marketing. Como acontece na maioria dos sites, recolhemos informações automaticamente e as salvamos em arquivos. Nessas informações, estão inclusos o endereço de IP, tipo de browser e idioma, provedor de serviço, páginas de referência, sistema operacional, data/hora e informações de cliques no site. Usamos essas informações para entender e analisar tendências, administrar o site, aprender sobre o comportamento dos usuários e reunir informações demográficas sobre nossa base de usuários como um todo.
+
+          Quizzes and Contests
+          Podemos dar a oportunidade aos alunos e visitantes de participarem de quizzes (testes) ou concursos em nosso site. Caso você participe, pediremos informações e dados pessoais. A participação em quizzes e concursos é completamente voluntária e, portanto, você tem a opção de não fornecer suas informações. As informações pedidas costumam incluir informações de contato (como nome e e-mail) e dados demográficos (como CEP). Usamos essas informações para personalizar os comunicados enviados a você e sugerir produtos e serviços mais relevantes.
+
+          Links para outros websites
+          Este site contém links para outros sites. Por favor, esteja ciente de que não somos responsáveis pelos sistemas de privacidade de outros sites. Nós incentivamos nossos usuários a serem cautelosos ao saírem do nosso site e recomendamos que leiam os termos de uso de qualquer website que colete informação pessoal identificável.
+        </p>
+
+      </main>
+
+    </>
+  );
+}
+
+export default Policy;
+
